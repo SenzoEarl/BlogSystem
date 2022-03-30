dcd439a50eeb7ceaef4a6f696b43f9fee1a46b54
Senzo Maseko <senzo.e.maseko@gmail.com>
2022-03-30 19:20:22 +0200

    Initial commit

--- BlogSystem/settings.py
+++ BlogSystem/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4tozx5yj1x+!nw*j1r8w#8jjip##((qz=juh(06)4$@#(x6w89'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
